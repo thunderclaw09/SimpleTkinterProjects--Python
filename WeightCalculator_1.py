2296f4bd4f382bd6805e7b06a34c6b443aa0320b
Thunder <83921358+thunderclaw09@users.noreply.github.com>
2022-03-16 19:46:06 -0400

    Changed the code so that the output system works now.

--- WeightCalculator_1.py
+++ WeightCalculator_1.py
@@ -7,12 +7,14 @@ class MyGUI:
         self.frm = ttk.Frame(self.root, padding=40)
         self.frm.grid()
 
+        self.Answer = ttk.Label(self.frm, text=" ")
+        self.Answer.grid(column=0, row=5)
         self.InputNum = ttk.Entry(self.frm, width=40)
         self.InputNum.grid(row=2)
 
         self.label1 = ttk.Label(self.frm, text="Please input your weight on earth, and the").grid(column=0, row=0)
         self.label2 = ttk.Label(self.frm, text="program will tell you your weight on Mars.").grid(column=0, row=1)
-        self.Answer = ttk.Label(self.frm, text="").grid(column=0, row=5)
+        
         self.Close = ttk.Button(self.frm, text="Quit", command=self.root.destroy).grid(column=3, row=5)
         self.Enter = ttk.Button(self.frm, text="Enter", command=self.EnterFunction).grid(column=3, row=2)
 
@@ -22,13 +24,21 @@ class MyGUI:
 
     def EnterFunction(self):
         #global Output
+
         print("Enter pressed!")
         Number = int(self.InputNum.get())
         NewNumber = (Number / 100) * 38
         print("Your weight on Mars:", NewNumber)
-        Output = "Your weight on Mars:", NewNumber
-        self.Answer["text"] = "This should work."
+        #Output = ("Your weight on Mars:", NewNumber)
+        Output = str(NewNumber)
+
+        # self.Answer['text'] = "This should work."              DID NOT WORK.
+        # self.Answer.config(text="This should work.")           ACTUALLY WORKED!   
+        # self.Answer.config(text=Output)                        WORKED, BUT IT LOOKED WEIRD
+        self.Answer.config(text="Your weight on Mars: "+Output)  # FINALLY WORKED AND LOOKED NICE!
+
         print("It should have worked.")
+
 
     
     
@@ -38,42 +48,3 @@ class MyGUI:
   
 
 myGUI = MyGUI()
-
-
-
-
-#########################################################################################
-#########################################################################################
-######   Tadam: this was my original code, when it was not included in a class:    ######
-#########################################################################################
-#########################################################################################
-
-# root = Tk()
-# frm = ttk.Frame(root, padding=40)
-# frm.grid()
-
-# InputNum = ttk.Entry(frm, width=40)
-# InputNum.grid(row=2)
-
-# # Output = ""
-
-# def EnterFunction():
-#     global Output
-#     print("Enter pressed!")
-#     Number = int(InputNum.get())
-#     NewNumber = (Number / 100) * 38
-#     print("Your weight on Mars:", NewNumber)
-#     # Output = "Your weight on Mars:", NewNumber
-#     Answer.config(text="This should work.")
-#     print("It should have worked.")
-
-    
-# ttk.Label(frm, text="Please input your weight on earth, and the").grid(column=0, row=0)
-# ttk.Label(frm, text="program will tell you your weight on Mars.").grid(column=0, row=1)
-# Answer = ttk.Label(frm, text="").grid(column=0, row=5)
-# Close = ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=5)
-# Enter = ttk.Button(frm, text="Enter", command=EnterFunction).grid(column=3, row=2)
-
-
-
-# root.mainloop()
